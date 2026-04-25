344702af119a92b26ceb9037d5a02bdaf0f05ac9
katrinebern <342857@via.dk>
2026-04-25 14:11:19 +0200

    Remove index.css and fix of spelling error

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -5,7 +5,7 @@ function AboutPage() {
       <p>This app was made for WEB2 Hand In 2.</p>
       <p>
         This app shows basic facts about countries. The app can also be useful
-        as a ressource library for the Global Business Engineering course:
+        as a resource library for the Global Business Engineering course:
         "International Business Communication."
       </p>
       <p>The app uses React, Vite, React Router and the REST Countries API.</p>
